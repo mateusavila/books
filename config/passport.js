682467ea95ebbafe61b02151861821b221e48aef
Leandro Oriente <leandroriente@gmail.com>
2013-12-14 00:33:52 -0200

    Vincula oauth a conta caso exista

--- config/passport.js
+++ config/passport.js
@@ -143,22 +143,32 @@ module.exports = function(passport) {
                 if (user) {
                     return done(null, user); // user found, return that user
                 } else {
-                    // if there is no user found with that facebook id, create them
-                    var newUser            = new User();
 
-                    // set all of the facebook information in our user model
-                    newUser.facebook.id    = profile.id; // set the users facebook id                   
-                    newUser.facebook.token = token; // we will save the token that facebook provides to the user                    
-                    newUser.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName; // look at the passport user profile to see how names are returned
-                    newUser.facebook.email = profile.emails[0].value; // facebook can return multiple emails so we'll take the first
+                    // Checa se usuário já existe na base com email fornecido pelo FB, se tiver atualiza conta
+                    User.find({ "local.email" : profile.emails[0].value}, function(err, user){
+                        var _user = user[0];
 
-                    // save our user to the database
-                    newUser.save(function(err) {
-                        if (err)
-                            throw err;
+                        if (!_user) {
+                            _user = new User();
+                            _user.local.name = profile.displayName;
+                            _user.local.email = profile.emails[0].value;
+                            // RANDOM 6 DIGISTS PASSWORD
+                            _user.hashPassword(Math.floor(Math.random()*90000) + 100000);
+                        }
 
-                        // if successful, return the new user
-                        return done(null, newUser);
+                        _user.facebook.id    = profile.id; // set the users facebook id                   
+                        _user.facebook.token = token; // we will save the token that facebook provides to the user                    
+                        _user.facebook.name  = profile.displayName; // look at the passport user profile to see how names are returned
+                        _user.facebook.email = profile.emails[0].value; // facebook can return multiple emails so we'll take the first
+
+                        // save our user to the database
+                        _user.save(function(err) {
+                            if (err)
+                                throw err;
+
+                            // if successful, return the new user
+                            return done(null, _user);
+                        });
                     });
                 }
 
